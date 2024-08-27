521c4c79abce5392452bebb82eeb6e169d13693a
sagargoswami2001 <sagargoswami2762001@gmail.com>
2024-08-27 11:04:01 +0530

    Added aws-ec2

---
+++ aws-ec2/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "SampleServer"
+  }
+}
