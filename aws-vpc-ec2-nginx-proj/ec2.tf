fb40d82fe7e7ebea52947ac2a9d85c9141ebbcb7
sagargoswami2001 <sagargoswami2762001@gmail.com>
2024-09-08 16:04:03 +0530

    Added aws-vpc-ec2-nginx-proj

---
+++ aws-vpc-ec2-nginx-proj/ec2.tf
@@ -0,0 +1,19 @@
+
+# EC2 instance For Nginx setup
+resource "aws_instance" "nginxserver" {
+  ami                         = "ami-0182f373e66f89c85"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public-subnet.id
+  vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo yum install nginx -y
+            sudo systemctl start nginx
+            EOF
+
+  tags = {
+    Name = "NginxServer"
+  }
+}
---
+++ aws-vpc-ec2-nginx-proj/security_groups.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "nginx-sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  #Inbound rule for HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow access from any IP address
+  }
+
+  #Outbound rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-sg"
+  }
+}
